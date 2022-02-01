bbd052897fea3b2bbc0fe09b727399262910ba00
Md.Meherul Islam <meherulsust@gmail.com>
2022-02-01 23:49:20 +0600

    feature/footer : link has added

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,26 +1,31 @@
-import Image from 'next/image';
-import styles from '../../styles/Footer.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faTwitter, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
-
+import Image from "next/image";
+import styles from "../../styles/Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 const Footer = () => {
-   
-    return (
-      <>
-        <footer className={styles.footerArea}>
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-lg-3 col-md-6">
-                <div
-                  className={styles.singleFooterWidget}
-                  data-aos="fade-up"
-                  data-aos-delay={50}
-                  data-aos-duration={500}
-                  data-aos-once="true"
-                >
-                  <div className={styles.widgetLogo}>
-                    <a href="index.html">
+  return (
+    <>
+      <footer className={styles.footerArea}>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-3 col-md-6">
+              <div
+                className={styles.singleFooterWidget}
+                data-aos="fade-up"
+                data-aos-delay={50}
+                data-aos-duration={500}
+                data-aos-once="true"
+              >
+                <div className={styles.widgetLogo}>
+                  <Link href={"/"}>
+                    <a>
                       <Image
                         src="/assets/images/logo.png"
                         alt="Hidmona"
@@ -28,126 +33,150 @@ const Footer = () => {
                         height={100}
                       />
                     </a>
-                  </div>
-                  <p>To get exclusive updates and benefits.</p>
-                  <ul className={styles.widgetSocial}>
-                    <li>
-                      <a href="#" className={styles.socialIconStyle} target="_blank"
-                      >
+                  </Link>
+                </div>
+                <p>To get exclusive updates and benefits.</p>
+                <ul className={styles.widgetSocial}>
+                  <li>
+                    <Link href={"/"}>
+                      <a className={styles.socialIconStyle} target="_blank">
                         <FontAwesomeIcon icon={faFacebookF} />
                       </a>
-                    </li>
-                    <li>
-                      <a href="#" className={styles.socialIconStyle} target="_blank">
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/"}>
+                      <a className={styles.socialIconStyle} target="_blank">
                         <FontAwesomeIcon icon={faTwitter} />
                       </a>
-                    </li>
-                    <li>
-                      <a href="#" className={styles.socialIconStyle} target="_blank">
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/"}>
+                      <a className={styles.socialIconStyle} target="_blank">
                         <FontAwesomeIcon icon={faInstagram} />
                       </a>
-                    </li>
-                    <li>
-                      <a href="#" className={styles.socialIconStyle} target="_blank">
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/"}>
+                      <a className={styles.socialIconStyle} target="_blank">
                         <FontAwesomeIcon icon={faLinkedinIn} />
                       </a>
-                    </li>
-                  </ul>
-                </div>
+                    </Link>
+                  </li>
+                </ul>
               </div>
-              <div className="col-lg-3 col-md-6">
-                <div
-                  className={styles.singleFooterWidget}
-                  data-aos="fade-up"
-                  data-aos-delay={60}
-                  data-aos-duration={600}
-                  data-aos-once="true"
-                >
-                  <h3>Usefull Links</h3>
-                  <ul className={styles.quickLinks}>
-                    <li>
-                      <a href="index.html">Home</a>
-                    </li>
-                    <li>
-                      <a href="faq.html">FAQ's</a>
-                    </li>
-                    <li>
-                      <a href="privacy-policy.html">Privacy Policy</a>
-                    </li>
-                    <li>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <div
+                className={styles.singleFooterWidget}
+                data-aos="fade-up"
+                data-aos-delay={60}
+                data-aos-duration={600}
+                data-aos-once="true"
+              >
+                <h3>Usefull Links</h3>
+                <ul className={styles.quickLinks}>
+                  <li>
+                    <Link href={"/"}>
+                      <a>Home</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/faq"}>
+                      <a>FAQ's</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/privacy-policy"}>
+                      <a>Privacy Policy</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/contact"}>
                       <a href="contact.html">Contact Us</a>
-                    </li>
-                  </ul>
-                </div>
+                    </Link>
+                  </li>
+                </ul>
               </div>
-              <div className="col-lg-3 col-md-6">
-                <div
-                  className={styles.singleFooterWidget}
-                  data-aos="fade-up"
-                  data-aos-delay={70}
-                  data-aos-duration={700}
-                  data-aos-once="true"
-                >
-                  <h3>Quick Links</h3>
-                  <ul className={styles.quickLinks}>
-                    <li>
-                      <a href="index.html">Home</a>
-                    </li>
-                    <li>
-                      <a href="faq.html">FAQ's</a>
-                    </li>
-                    <li>
-                      <a href="privacy-policy.html">Privacy Policy</a>
-                    </li>
-                    <li>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <div
+                className={styles.singleFooterWidget}
+                data-aos="fade-up"
+                data-aos-delay={70}
+                data-aos-duration={700}
+                data-aos-once="true"
+              >
+                <h3>Quick Links</h3>
+                <ul className={styles.quickLinks}>
+                  <li>
+                    <Link href={"/"}>
+                      <a>Home</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/faq"}>
+                      <a>FAQ's</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/privacy-policy"}>
+                      <a>Privacy Policy</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/contact"}>
                       <a href="contact.html">Contact Us</a>
-                    </li>
-                  </ul>
-                </div>
+                    </Link>
+                  </li>
+                </ul>
               </div>
-              <div className="col-lg-3 col-md-6">
-                <div
-                  className={styles.singleFooterWidget}
-                  data-aos="fade-up"
-                  data-aos-delay={80}
-                  data-aos-duration={800}
-                  data-aos-once="true"
-                >
-                  <h3>Contact Us</h3>
-                  <ul className={styles.infoLinks}>
-                    <li>
-                      <span>Support (USA) : +1 607 354 4284</span>
-                    </li>
-                    <li>
-                      <span>Support (UK) : +44 1244 94 0789</span>
-                    </li>
-                    <li>
-                      <span>Support(Sweden) : +46 40-693 98 96</span>
-                    </li>
-                    <li>
-                      <span>Email:</span>support@hidmona.ch
-                    </li>
-                  </ul>
-                </div>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <div
+                className={styles.singleFooterWidget}
+                data-aos="fade-up"
+                data-aos-delay={80}
+                data-aos-duration={800}
+                data-aos-once="true"
+              >
+                <h3>Contact Us</h3>
+                <ul className={styles.infoLinks}>
+                  <li>
+                    <span>Support (USA) : +1 607 354 4284</span>
+                  </li>
+                  <li>
+                    <span>Support (UK) : +44 1244 94 0789</span>
+                  </li>
+                  <li>
+                    <span>Support(Sweden) : +46 40-693 98 96</span>
+                  </li>
+                  <li>
+                    <span>Email:</span>support@hidmona.ch
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
-          <div className={styles.copyrightArea}>
-            <div className="container">
-              <div className={styles.copyrightAreaContent}>
-                <p>
-                  Copyright © 2022 Hidmona Money Transfer - All Rights Reserved
-                  <a href="#" target="_blank"></a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </footer>
-        <div className="go-top">
-          <i className="ri-arrow-up-s-line" />
         </div>
-      </>
-    );
+        <div className={styles.copyrightArea}>
+          <div className="container">
+            <div className={styles.copyrightAreaContent}>
+              <p>
+                Copyright © 2022 Hidmona Money Transfer - All Rights Reserved
+                <a href="#" target="_blank"></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div className="go-top">
+        <i className="ri-arrow-up-s-line" />
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,56 +1,76 @@
-import styles from '../../styles/Header.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
-import Image from 'next/image';
+import styles from "../../styles/Header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 const Header = () => {
-    const {topBarArea, topBarInformation, topBarAction} = styles;
+  const { topBarArea, topBarInformation, topBarAction } = styles;
 
-    return (
-        <div className={topBarArea}>
-            <div className='container-fluid'>
-                <div className='row align-items-center'>
-                    <div className="col-lg-6 col-md-6">
-                        <ul className={topBarInformation}>
-                            <li>
-                                <a href="tel:+1 607 354 4284">+1 607 354 4284 </a>
-                            </li>
-                            <li>
-                                <span style={{color:"#fff"}}>support@hidmona.ch</span>
-                            </li>
-                        </ul>
-                    </div>
+  return (
+    <div className={topBarArea}>
+      <div className="container-fluid">
+        <div className="row align-items-center">
+          <div className="col-lg-6 col-md-6">
+            <ul className={topBarInformation}>
+              <li>
+                <a href="tel:+1 607 354 4284">+1 607 354 4284 </a>
+              </li>
+              <li>
+                <span style={{ color: "#fff" }}>support@hidmona.ch</span>
+              </li>
+            </ul>
+          </div>
 
-                    <div className="col-lg-6 col-md-6">
-                        <ul className={topBarAction}>
-                            <li>
-                                <a href="help-center.html">Help</a>
-                            </li>
-                            <li className="dropdown language-option">
-                                <button className="dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-haspopup="true"
-                                    aria-expanded="false">
-                                    <FontAwesomeIcon icon={faGlobe} />
-                                    <span className="lang-name">English</span>
-                                </button>
-                                <div className="dropdown-menu language-dropdown-menu">
-                                    <a className="dropdown-item" href="#">
-                                        <Image src='/assets/images/uk.png' alt='' width={10} height={10}/>
-                                        English
-                                    </a>
-                                    <a className="dropdown-item" href="#">
-                                        <Image src='/assets/images/china.png' alt='' width={10} height={10}/>
-                                        简体中文
-                                    </a>
-                                    <a className="dropdown-item" href="#">
-                                        <Image src='/assets/images/uae.png' alt='' width={10} height={10}/>
-                                        العربيّة
-                                    </a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+          <div className="col-lg-6 col-md-6">
+            <ul className={topBarAction}>
+              <li>
+                <a href="help-center.html">Help</a>
+              </li>
+              <li className="dropdown language-option">
+                <button
+                  className="dropdown-toggle "
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <FontAwesomeIcon icon={faGlobe} />
+                  <span className="lang-name">English</span>
+                </button>
+                <div className="dropdown-menu language-dropdown-menu">
+                  <a className="dropdown-item" href="#">
+                    <Image
+                      src="/assets/images/uk.png"
+                      alt=""
+                      width={10}
+                      height={10}
+                    />
+                    English
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    <Image
+                      src="/assets/images/china.png"
+                      alt=""
+                      width={10}
+                      height={10}
+                    />
+                    简体中文
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    <Image
+                      src="/assets/images/uae.png"
+                      alt=""
+                      width={10}
+                      height={10}
+                    />
+                    العربيّة
+                  </a>
                 </div>
-            </div>
+              </li>
+            </ul>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 export default Header;
